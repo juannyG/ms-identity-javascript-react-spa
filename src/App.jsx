8f387d5c0c1cefe3469bf8d0a957d90f7eabaf6e
Juan Gutierrez <juanny.gee@gmail.com>
2022-11-07 17:40:07 -0500

    Get the token ID and put it up on the page

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,40 @@ import { callMsGraph } from "./graph";
 import Button from "react-bootstrap/Button";
 import "./styles/App.css";
 
+
+const IdTokenContent = () => {
+    const { instance, accounts } = useMsal();
+    const [ idToken, setIdToken ] = useState(null);
+
+    // Silently acquires an access token which is then attached to a request for MS Graph data
+    instance.acquireTokenSilent({
+        ...loginRequest,
+        account: accounts[0]
+    }).then((response) => {
+        setIdToken(response.idToken);
+    });
+
+    return (
+        <>
+          {idToken &&
+           <div style={{maxWidth: "450px", wordBreak: "break-all"}}>
+             <div>ID TOKEN</div>
+             <div> {idToken} </div>
+           </div>
+          }
+        </>
+    );
+};
+
 /**
  * Renders information about the signed-in user or a button to retrieve data about the user
  */
 const ProfileContent = () => {
     const { instance, accounts } = useMsal();
-    const [graphData, setGraphData] = useState(null);
-
-    function RequestProfileData() {
-        // Silently acquires an access token which is then attached to a request for MS Graph data
-        instance.acquireTokenSilent({
-            ...loginRequest,
-            account: accounts[0]
-        }).then((response) => {
-            callMsGraph(response.accessToken).then(response => setGraphData(response));
-        });
-    }
 
     return (
         <>
             <h5 className="card-title">Welcome {accounts[0].name}</h5>
-            {graphData ? 
-                <ProfileData graphData={graphData} />
-                :
-                <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
-            }
         </>
     );
 };
@@ -39,11 +48,12 @@ const ProfileContent = () => {
 /**
  * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
  */
-const MainContent = () => {    
+const MainContent = () => {
     return (
         <div className="App">
             <AuthenticatedTemplate>
                 <ProfileContent />
+                <IdTokenContent />
             </AuthenticatedTemplate>
 
             <UnauthenticatedTemplate>
